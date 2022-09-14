e8dbe4382bd38187ff3117c2b5977f37b887c983
db <jacboysent@gmail.com>
2022-09-14 17:40:19 -0500

    added due color function

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,9 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check the due date
+  auditTask(taskLi);
+
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -133,14 +136,24 @@ $(".list-group").on("click", "span", function() {
     .attr("type", "text")
     .addClass("form-control")
     .val(date);
+
   $(this).replaceWith(dateInput);
+
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      // when the calender is closed, force a "change" event on the 'dateInput'
+      $(this).trigger("change");
+    }
+  })
 
   // automatically bring up the calendar
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   var date = $(this).val();
 
   // get status type and position in the list
@@ -161,6 +174,9 @@ $(".list-group").on("blur", "input[type='text']", function() {
     .addClass("badge badge-primary badge-pill")
     .text(date);
   $(this).replaceWith(taskSpan);
+
+  // pass the task's <li> element into auditTask() to check the new due date
+  auditTask($(taskSpan).closest("list-group-item"));
 });
 
 // remove all tasks
@@ -224,6 +240,39 @@ $("#trash").droppable({
     console.log("out");
   }
 });
+
+// enable datepicker
+
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
+
+// audit task function
+
+var auditTask = function(taskEl) {
+  // get the date from the task element.
+  var date = $(taskEl).find("span").text().trim();
+
+  // convert it to moment object at 5:00 PM
+  var time = moment(date, "L").set("hour", 17);
+
+  // remove any old classes from the element.
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } 
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
+
 
 // load tasks for the first time
 loadTasks();
